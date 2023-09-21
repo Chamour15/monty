ef31f580cd25e8c5284b7f3882a0d3bbfeccd311
Chamour15 <wafae.chamour@gmail.com>
2023-09-21 11:37:32 -0700

    Add main.c & monty.h

---
+++ monty.h
@@ -0,0 +1,18 @@
+#ifndef MONTY_H
+#define MONTY_H
+
+#define _GNU_SOURCE
+#define BUFF_SIZE 256
+#define BUFFER 128
+#define UNUSED(x) ((void)(x))
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <ctype.h>
+
+#endif
+
